690fb14bba6157400bf605d9b4038535b5eff71d
Jainethin-M <jainethin59@gmail.com>
2024-12-27 21:10:53 +0530

    link

--- frontend/src/constants/url.js
+++ frontend/src/constants/url.js
@@ -1,3 +1,3 @@
-// export const baseURL =  'https://kpr-event-admin.onrender.com'
-export const baseURL =  ( process.env.APP_ON !=='production' ?'http://localhost:9001' : 'https://kpr-event-admin.onrender.com')
+export const baseURL =  'https://kpr-event-admin.onrender.com'
+// export const baseURL =  'http://localhost:9001' 
 export const eventURL = 'https://event-kpr.onrender.com'
