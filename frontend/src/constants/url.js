4eac93cd722305877e6566f2eed876426c7b9e44
Jainethin-M <jainethin59@gmail.com>
2024-12-27 18:25:27 +0530

    final deploy commit

--- frontend/src/constants/url.js
+++ frontend/src/constants/url.js
@@ -1,2 +1,2 @@
-export const baseURL = 'http://localhost:9001'
+export const baseURL = process.env.APP_ON !=='production' ? 'http://localhost:9001' : 'https://kpr-event-admin.onrender.com'
 export const eventURL = 'https://event-kpr.onrender.com'
